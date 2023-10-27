6f9120e4bb6a569a2ec67f9c0df887abd1a31aaa
Duyoofmp <duyoofibnudileep@gmail.com>
2023-10-27 19:34:25 +0530

    new

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,51 +1,56 @@
 import React, { useState } from "react";
 import "./Home.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 
-const Home   = () => {
-  const [inputText, setInputText] = useState("");
-  const [chatHistory, setChatHistory] = useState([]);
+const data = [
+  {
+    
+    uv: 0.423,
+    pv: 0.698,
+    amt: 2400
+  },
+  {
+    
+    uv: 0.256,
+    pv: 0.549,
+    amt: 2210
+  },
+ 
+];
 
-  const handleInputChange = (e) => {
-    setInputText(e.target.value);
-  };
-
-  const handleVoiceInput = () => {
-    // Implement voice input functionality here
-  };
-
-  const handleSendClick = () => {
-    if (inputText.trim() === "") return;
-    const newChat = {
-      text: inputText,
-      date: new Date().toLocaleString(),
-    };
-    setChatHistory([...chatHistory, newChat]);
-    setInputText("");
-  };
-
+export default function Home() {
   return (
-    <div className="chat-container">
-      <div className="chat-sidebar">
-        {chatHistory.map((chat, index) => (
-          <div key={index} className="chat-history-item">
-            {chat.date}: {chat.text}
-          </div>
-        ))}
-      </div>
-      <div className="chat-main">
-        <div className="input-container">
-          <input
-            type="text"
-            value={inputText}
-            onChange={handleInputChange}
-            placeholder="Type your message..."
-          />
-          <button onClick={handleVoiceInput}>🎤</button>
-          <button onClick={handleSendClick}>Send</button>
-        </div>
-      </div>
-    </div>
+    <LineChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line
+        type="monotone"
+        dataKey="pv"
+        stroke="#8884d8"
+        activeDot={{ r: 8 }}
+      />
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+    </LineChart>
   );
-};
-
-export default Home;
+}
